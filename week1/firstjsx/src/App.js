1b9666be5d0932d01c19dcfc81285803f90be349
angela bereski <angelakbereski@gmail.com>
2022-10-27 12:49:16 -0600

    firstjsx

---
+++ week1/firstjsx/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+      </header>
+      <main>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dog</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
